8ad78a12d5ce4bd722b3e92d6ab012784e7c516b
Joao Paulo Raposo <raposojoaopaulo@gmail.com>
2022-09-14 13:49:19 -0300

    Initial commit with html and css created

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BsSearch, BsPinMapFill, BsDroplet, BsWind } from 'react-icons/bs';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="form">
+          <h3>check the weather of a city now</h3>
+          <div className="form-input-container">
+            <input type="text" placeholder="city name" id='city-input' />
+            <button id="search">
+              <BsSearch />
+            </button>
+          </div>
+        </div>
+        <div id="weather-data">
+          <h2>
+            <BsPinMapFill />
+            <span id="city-name"> Cidade teste</span>
+            <img 
+              src="https://countryflagsapi.com/png/br"
+              alt="Country Flag"
+              id="country"
+            />
+          </h2>
+          <p id='temperature'><span>38</span>&deg;C</p>
+          <div id="description-container">
+            <p id="description">description</p>
+            <img 
+              src="http://openweathermap.org/img/wn/02d.png"
+              alt="Weather condition"
+              id="weather-icon"
+            />            
+          </div>
+          <div id="details-container">
+            <p id="humidity">
+              <BsDroplet /> <span>50</span>%              
+            </p>
+            <p id="wind">
+              <BsWind /> <span>15km/h</span>           
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/providers/openWeatherApi.ts
@@ -0,0 +1,1 @@
+export const apiKey = 'c25b219632d2ad8fe175c56c680fa45f';
