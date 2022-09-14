8174aa06839f07219df6be005f8c99d3423d1453
Joao Paulo Raposo <raposojoaopaulo@gmail.com>
2022-09-14 16:12:14 -0300

    show weather for city

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,60 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import { BsSearch, BsPinMapFill, BsDroplet, BsWind } from 'react-icons/bs';
+import { apiWeather, apiCountry } from './providers/openWeatherApi';
 
 function App() {
+  const [city, setCity] = useState('');
+  const [error, setError] = useState(false);
+
+  const [cityName, setCityName] = useState('');
+  const [country, setCountry] = useState('');
+  const [temp, setTemp] = useState('');
+  const [description, setDescription] = useState('');
+  const [icon, setIcon] = useState('');
+  const [humidity, setHumidity] = useState('');
+  const [wind, setWind] = useState('');
+
+
+  const handleSearch = (e: any) => {
+    e.preventDefault();
+    getWeaterData(city);
+  };
+
+  const getWeaterData = async (city: string) => {
+    const response = await fetch(apiWeather(city));
+    const data = await response.json();
+
+    console.log(data);
+
+    if (data.cod === 200) {
+      setError(false);
+      setCityName(data.name);
+      setCountry(data.sys.country);
+      setDescription(data.weather[0].description);
+      setIcon(data.weather[0].icon);
+      setTemp(parseInt(data.main.temp).toString());
+      setHumidity(data.main.humidity);
+      setWind(data.wind.speed);
+    } else {
+      setError(true);
+    }
+  }; 
+
   return (
     <div className="App">
       <div className="container">
         <div className="form">
           <h3>check the weather of a city now</h3>
           <div className="form-input-container">
-            <input type="text" placeholder="city name" id='city-input' />
-            <button id="search">
+            <input 
+              type="text" 
+              placeholder="city name" 
+              id='city-input' 
+              onChange={(e) => setCity(e.target.value)}
+            />
+            <button id="search" onClick={handleSearch} >
               <BsSearch />
             </button>
           </div>
@@ -18,28 +62,28 @@ function App() {
         <div id="weather-data">
           <h2>
             <BsPinMapFill />
-            <span id="city-name"> Cidade teste</span>
+            <span id="city-name">{cityName}</span>
             <img 
-              src="https://countryflagsapi.com/png/br"
+              src={`https://countryflagsapi.com/png/${country}`}
               alt="Country Flag"
               id="country"
             />
           </h2>
-          <p id='temperature'><span>38</span>&deg;C</p>
+          <p id='temperature'><span>{temp}</span>&deg;C</p>
           <div id="description-container">
-            <p id="description">description</p>
+            <p id="description">{description}</p>
             <img 
-              src="http://openweathermap.org/img/wn/02d.png"
+              src={`http://openweathermap.org/img/wn/${icon}.png`}
               alt="Weather condition"
               id="weather-icon"
             />            
           </div>
           <div id="details-container">
             <p id="humidity">
-              <BsDroplet /> <span>50</span>%              
+              <BsDroplet /> <span>{humidity}</span>%              
             </p>
             <p id="wind">
-              <BsWind /> <span>15km/h</span>           
+              <BsWind /> <span>{wind}km/h</span>           
             </p>
           </div>
         </div>
--- src/providers/openWeatherApi.ts
+++ src/providers/openWeatherApi.ts
@@ -1,1 +1,9 @@
-export const apiKey = 'c25b219632d2ad8fe175c56c680fa45f';
+const apiKey = "c25b219632d2ad8fe175c56c680fa45f";
+
+export const apiWeather = (city: string) => {
+  return `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;  
+};
+
+export const apiCountry = (country: string) => {
+  return `https://countryflagsapi.com/png/${country}`
+};
